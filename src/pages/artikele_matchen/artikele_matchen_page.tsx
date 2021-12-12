1391bccb4882922cdaa2fae1c5d7e4c3900b95ba
Dominik Reinert <dominik.reinert.merzig@googlemail.com>
2021-12-12 11:22:43 +0100

    add randomizer to the words

--- src/pages/artikele_matchen/artikele_matchen_page.tsx
+++ src/pages/artikele_matchen/artikele_matchen_page.tsx
@@ -34,13 +34,12 @@ enum ResolvedState {
   INCORRECT = "INCORRECT",
 }
 const currentState: {
-  index: number;
   showTranslation: boolean;
   resolvedState: ResolvedState;
+  currentWord?: Word;
   resolvedArtikele?: Artikele;
   filteredWords: () => Word[];
 } = proxy({
-  index: 0,
   filteredWords: () =>
     wordStore.getCurrentDataAdapted().words.filter((word) =>
       filterState.booksAndChapters
@@ -62,27 +61,43 @@ const currentState: {
   resolvedState: ResolvedState.OPEN,
 });
 
+/**
+ * Randomize array in-place using Durstenfeld shuffle algorithm
+ *
+ * Credits to https://stackoverflow.com/a/12646864/3643533
+ */
+function shuffleArray<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 const ArtikeleMatchenPageSuspending = () => {
   const styleContext = useStyleContext();
   useUpdateOnStoreChange(wordStore);
 
   useSnapshot(filterState);
   const componentState = useSnapshot(currentState);
-  const currentWord = componentState.filteredWords()[componentState.index];
+
+  function getRandomWord(): Word {
+    return shuffleArray(componentState.filteredWords().slice(0))[0];
+  }
+
+  if (componentState.currentWord === undefined) {
+    currentState.currentWord = getRandomWord();
+  }
 
   function transitionToOpen(): void {
     currentState.resolvedState = ResolvedState.OPEN;
     currentState.resolvedArtikele = undefined;
     currentState.showTranslation = false;
-    currentState.index++;
+    currentState.currentWord = getRandomWord();
   }
 
   function showTranslation(): void {
     currentState.showTranslation = true;
-  }
-
-  function restart(): void {
-    currentState.index = 0;
   }
 
   const resolvedStateClass =
@@ -103,16 +118,15 @@ const ArtikeleMatchenPageSuspending = () => {
       <div className="width-wrapper">
         <div className={`word-card ${resolvedStateClass}`}>
           <div className="help">
-            <span onClick={() => restart()}>nei starten</span>
             <span onClick={() => showTranslation()}>?</span>
           </div>
           <div className="word">
-            <span>{currentWord?.singular}</span>
+            <span>{componentState.currentWord?.singular}</span>
           </div>
           <div className="translation">
             <span>
               {componentState.showTranslation &&
-                `(${currentWord?.translation})`}
+                `(${componentState.currentWord?.translation})`}
             </span>
           </div>
         </div>
@@ -137,8 +151,6 @@ function ArtikeleOption(props: {
   resolvedStateClass: string;
 }): JSX.Element {
   const componentState = useSnapshot(currentState);
-  const words = wordStore.getCurrentDataAdapted().words;
-  const currentWord = words[componentState.index];
 
   function transitionToIncorrect(artikele: Artikele): void {
     currentState.resolvedState = ResolvedState.INCORRECT;
@@ -153,19 +165,25 @@ function ArtikeleOption(props: {
   function handleArtikeleClick(artikele: Artikele): void {
     switch (artikele) {
       case Artikele.D:
-        [Sex.WEIBLECH, Sex.SAECHLECH].includes(currentWord.sex)
+        [Sex.WEIBLECH, Sex.SAECHLECH].includes(
+          componentState.currentWord?.sex as Sex
+        )
           ? transitionToCorrect(artikele)
           : transitionToIncorrect(artikele);
         break;
       case Artikele.DE:
-        currentWord.sex === Sex.MAENNLECH &&
-        !nAllowedRule.includes(currentWord.singular[0].toLocaleLowerCase())
+        componentState.currentWord?.sex === Sex.MAENNLECH &&
+        !nAllowedRule.includes(
+          componentState.currentWord.singular[0].toLocaleLowerCase()
+        )
           ? transitionToCorrect(artikele)
           : transitionToIncorrect(artikele);
         break;
       case Artikele.DEN:
-        currentWord.sex === Sex.MAENNLECH &&
-        nAllowedRule.includes(currentWord.singular[0].toLocaleLowerCase())
+        componentState.currentWord?.sex === Sex.MAENNLECH &&
+        nAllowedRule.includes(
+          componentState.currentWord.singular[0].toLocaleLowerCase()
+        )
           ? transitionToCorrect(artikele)
           : transitionToIncorrect(artikele);
     }
