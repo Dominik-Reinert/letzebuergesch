542b596af53f8402197d1451cb8209560cc34479
DoRex <dore250393@gmail.com>
2021-12-14 15:30:45 +0100

    show the filter dropdown conditionally

--- src/navbar/navbar.tsx
+++ src/navbar/navbar.tsx
@@ -1,17 +1,24 @@
 /**@jsx jsx */
 import { jsx } from "@emotion/react";
+import { useLocation } from "react-router";
 import { FilterDropdown } from "../filter/filter_dropdown";
+import { Routes } from "../routes/routes";
 import { useStyleContext } from "../style_context/use_style_context";
 import { navbarStyle } from "./navbar_style";
 
 export function Navbar(): JSX.Element {
   const styleContext = useStyleContext();
+  const location = useLocation();
+
+  function shouldRenderFilterDropdown(): boolean {
+    return location.pathname.includes(Routes.ARTIKELE_MATCHEN);
+  }
 
   return (
     <div css={navbarStyle(styleContext)}>
       <div className="page-name-wrapper">
         <span className="page-name">Lëtzebuergesch</span>
-        <FilterDropdown />
+        {shouldRenderFilterDropdown() && <FilterDropdown />}
       </div>
     </div>
   );
