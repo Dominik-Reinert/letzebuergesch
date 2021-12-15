27abb754d986f5b8ecc7dc4e0f9acfc0ddc4a543
DoRex <dore250393@gmail.com>
2021-12-15 08:38:41 +0100

    fix missing initialize flag

--- src/filter/filter_dropdown.tsx
+++ src/filter/filter_dropdown.tsx
@@ -56,6 +56,7 @@ export function FilterDropdown(): JSX.Element {
   React.useEffect(() => {
     if (!componentState.initialized) {
       updateFiltersOnStoreUpdate();
+      filterState.initialized = true;
     }
   }, [updateFiltersOnStoreUpdate]);
 
