dbe450a59f64ae1b07bed8b315e3e5f63b31a234
Dominik Reinert <dominik.reinert.merzig@gmail.com>
2021-12-26 08:47:01 +0100

    remove shuffle array function which is unused by now

--- src/pages/artikele_matchen/artikele_matchen_page.tsx
+++ src/pages/artikele_matchen/artikele_matchen_page.tsx
@@ -71,18 +71,6 @@ const currentState: {
   resolvedState: ResolvedState.OPEN
 });
 
-/**
- * Randomize array in-place using Durstenfeld shuffle algorithm
- *
- * Credits to https://stackoverflow.com/a/12646864/3643533
- */
-function shuffleArray<T>(array: T[]): T[] {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
 
 const ArtikeleMatchenPageSuspending = () => {
   const styleContext = useStyleContext();
